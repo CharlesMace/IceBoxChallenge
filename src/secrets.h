f5dc523e25539da8b6564589a62aff8b269f8fb2
Charles Mace <55921248+CharlesMace@users.noreply.github.com>
2022-01-31 17:19:35 -0300

    I've hiden the stuff you don't want to know

--- src/secrets.h
+++ src/secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "yhello"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "mylifeispotato"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "***********"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "***********"	// replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 1637499			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "TH5742FC0FV6KXI1"   // replace XYZ with your channel write API Key
+#define SECRET_CH_ID **************			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "******************"   // replace XYZ with your channel write API Key
